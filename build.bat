6db3ad2d56202c667ef5a347be14b27ceb3aaf85
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-09-12 16:52:27 +0200

    fixed issue with build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 python -m pip install -r requirements.txt
+if %ERRORLEVEL% neq 0 goto throw_error
 pyinstaller client.py --noconfirm
 mkdir dist\client\assets
 xcopy /s assets\ dist\client\
+
+exit /b 0
+
+:throw_error
+echo "maaad sadge, install python and try again. If this keeps happening try installing the packages manually from the 'requirements.txt' file"
+exit /b 1
