c45584d1f88a9c0ed087bec5a193d3facf3ea0e4
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-09-12 16:44:50 +0200

    fixed windows build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 python -m pip install -r requirements.txt
 pyinstaller client.py --noconfirm
-mkdir dist/client/assets
-xcopy /s/y assets/* dist/client/assets/
+mkdir dist\client\assets
+xcopy /s assets\ dist\client\
