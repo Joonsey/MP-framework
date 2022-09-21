f2e0c193227921bb63af5db03c958b110e784dc1
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-09-21 18:07:26 +0200

    improved build to windows and unix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 python -m pip install -r requirements.txt
-python -m pyinstaller client.py --noconfirm -F
+python -m pyinstaller client.py --noconfirm -F --noconsole
 mkdir dist/assets
 cp assets/* dist/assets/
